3c6b7d5023cc92302b125c360f664bc1d5514ae6
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-11-01 20:30:01 -0300

    Index

--- resources/js/Pages/Posts/Post.jsx
+++ resources/js/Pages/Posts/Post.jsx
@@ -1,40 +1,7 @@
-import React, { useState, useRef } from "react";
-import Dropdown from "@/Components/Dropdown";
-import { useForm, usePage } from "@inertiajs/react";
-import PostForm from "./PostForm";
+import React  from "react";
 
 export default function Post({ post }) {
-    const { auth } = usePage().props;
-
-    const [editing, setEditing] = useState(false);
-
-    const { data, setData, patch, clearErrors, reset, errors,processing } = useForm({
-        titulo: post.titulo,
-        conteudo: post.conteudo,
-        imagem_destaque: post.imagem_destaque,
-    });
-
-    const submit = (e) => {
-        e.preventDefault();
-
-        console.log(data);
-
-        patch(route("posts.update", post.id), {
-            onSuccess: () => setEditing(false),
-        });
-    };
-
-    const cancel = () => {
-        if (window.confirm("Tem certeza de que deseja cancelar?")) {
-            // Executar ação de cancelamento
-            setEditing(false);
-            reset();
-            clearErrors();
-        }
-    };
-
-    const inputRef = useRef(); // Crie uma referência para o input de arquivo
-
+    
     return (
         <div className="p-6 flex flex-col">
             {" "}
@@ -59,44 +26,8 @@ export default function Post({ post }) {
                                 </small>
                             )}
                         </small>
-                    </div>
-                    {post.user.id === auth.user.id && (
-                        <Dropdown>
-                            <Dropdown.Trigger>
-                                <button>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-4 w-4 text-gray-400"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                    </svg>
-                                </button>
-                            </Dropdown.Trigger>
-                            <Dropdown.Content>
-                                <button
-                                    className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out"
-                                    onClick={() => setEditing(true)}
-                                >
-                                    Edit
-                                </button>
-                            </Dropdown.Content>
-                        </Dropdown>
-                    )}
-
-                    {editing ? (
-                        <PostForm
-                            data={data}
-                            errors={errors}
-                            setData={setData}
-                            inputRef={inputRef}
-                            submit={submit}
-                            cancel={cancel}
-                            processing={processing}
-                        />
-                    ) : (
-                        <div>
+                    </div>                    
+                        <div>   
                             <h1 className="text-xl font-semibold text-gray-900 mt-4">
                                 {post.titulo}
                             </h1>
@@ -104,8 +35,7 @@ export default function Post({ post }) {
                             <p className="mt-4 text-lg text-gray-900">
                                 {post.conteudo}
                             </p>
-                        </div>
-                    )}
+                        </div>                    
                 </div>
             </div>
         </div>
